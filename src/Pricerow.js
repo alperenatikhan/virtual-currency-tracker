564e5ba300430446e63560827fb8e3e84895f49e
alperenatikhan <alperenmyung@gmail.com>
2021-06-13 16:16:47 +0200

    4 files modified

--- src/Pricerow.js
+++ src/Pricerow.js
@@ -5,6 +5,7 @@ export default function Pricerow(props) {
   let [monthlyChange, setMonthlyChange] = useState('LOADING');
   let [annualChange, setAnnualChange] = useState('loading');
   let [base, setBase] = useState(['bitcoin']);
+  let [sparkline, setSparkline] = useState('');
 
   useEffect(() => {
     fetch(`https://api.coingecko.com/api/v3/coins/${props.name}?sparkline=true`)
@@ -24,22 +25,34 @@ export default function Pricerow(props) {
         setPrice(object.market_data.current_price);
         setMonthlyChange(object.market_data.price_change_percentage_30d);
         setAnnualChange(object.market_data.price_change_percentage_1y);
+        setSparkline(object.market_data.sparkline_7d.price);
       })
 
       .catch(error => console.log(error));
   }, []);
 
+  let generateGraph = item =>
+    `https://quickchart.io/chart?bkg=white&c={type:'sparkline',data:{datasets:[{fill:true,borderColor:'red',data:[${sparkline}]}]}}`;
+
   return (
     <tr>
       {' '}
-      <td> {props.name} </td> <td> {price.usd} USD</td>{' '}
+      <td>
+        {' '}
+        <tr>{props.name} </tr>{' '}
+        <tr>
+          {' '}
+          <img className="spark" src={generateGraph(sparkline)} />{' '}
+        </tr>{' '}
+      </td>{' '}
+      <td> {price.usd} USD</td>{' '}
       {monthlyChange >= 0 ? (
-        <td style={{ color: 'green' }}> {monthlyChange} % </td>
+        <td style={{ color: 'green' }}> {monthlyChange} </td>
       ) : (
-        <td style={{ color: 'red' }}> {monthlyChange} % </td>
+        <td style={{ color: 'red' }}> {monthlyChange} </td>
       )}
       {annualChange >= 0 ? (
-        <td style={{ color: 'green' }}> {annualChange} % </td>
+        <td style={{ color: 'green' }}> {annualChange} </td>
       ) : (
         <td style={{ color: 'red' }}> {annualChange} % </td>
       )}
